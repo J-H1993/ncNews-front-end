1c70d4ed98727d419a246a84bbd081082dccc496
john <jkhowarth93@gmail.com>
2024-06-17 16:27:09 +0100

    Made each article card clickable which links to the article on the card, added the individual article page to display the data, added the get article by id function into the api file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Header from "./components/Header"
 import Home from './pages/Home'
+import FullArticlePage from './pages/FullArticlePage'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/articles/:article_id" element={<FullArticlePage />} />
 
       </Routes>
     </div>
